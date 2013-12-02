475dc39bea69e6ca35e780a67caf2873502f70c0
sharath <sharath_bcs10@nitc.ac.in>
2013-12-02 21:19:26 +0530

    Constructor

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+public class Constructor
+	{String value;
+	public Constructor(String name)
+		{
+		value=name;
+		System.out.println(value);
+		}
+	public static void main(String []args)
+		{
+			Constructor sample=new Constructor("sharath");
+		}
+	}
+	
+		
