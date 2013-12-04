5b1aa2c37f284d01173ee2d140bd649e70a121c5
sharath <sharath_bcs10@nitc.ac.in>
2013-12-04 21:12:24 +0530

    ForLoop

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+public class Forloop{
+	
+public static void main(String []args)
+	{
+	
+	int x=Integer.parseInt(args[0]);
+	
+	for(;x<10;x=x+1)
+	{
+	System.out.print("Value of x = "+ x + "\n");
+	}
+	}
+}
