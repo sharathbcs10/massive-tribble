15f82cce056741e9d122727e9e89b2349cc67634
sharath <sharath_bcs10@nitc.ac.in>
2013-12-04 20:55:32 +0530

    Whileloop example

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop{
+	
+	public static void main(String []args){
+	
+	int x=Integer.parseInt(args[0]);
+	
+	while(x<10)
+	{
+	System.out.print("Value of x = " + x+"\n");
+	x++;
+	}
+}
+}
