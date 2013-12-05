64955b4d46d3acefddb0c36aa582bda9180548c8
sharath <sharath_bcs10@nitc.ac.in>
2013-12-05 12:46:47 +0530

    Sample Numbers class methods

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+public class Numbers{
+
+	
+
+	public static void main(String args[])
+		{
+		Integer x=3;
+		long a=x.longValue();
+		if(x.equals(3))
+		{
+		System.out.print("x = 3\n");
+		}
+		else
+		{
+		System.out.print("x Not equal to 3\n");
+		}
+		double z=x.doubleValue();
+		double b=Math.sqrt(z);
+		System.out.print(b+"\n");
+		System.out.print(a);
+		}}
