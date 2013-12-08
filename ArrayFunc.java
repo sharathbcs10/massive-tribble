d8ba5348165c6849e219b9091fe1d9b6507e1124
sharath <sharath_bcs10@nitc.ac.in>
2013-12-08 15:58:10 +0530

    Passing array to functions

---
+++ ArrayFunc.java
@@ -0,0 +1,17 @@
+public class ArrayFunc{
+
+	public static int [] array={1,2,4,5,7};
+
+	public static void main(String []args)
+	{
+	printarray(array);
+	}
+	
+	public static void printarray(int []array)
+	{
+	for(int i :array)
+	{
+	System.out.print(i+" ");
+	}
+	}
+	}
